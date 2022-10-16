b0ef4a7ef731648df63ebe00882c034ba06d0990
ismoilovdev <ismoilovdevarchlinux@gmail.com>
2022-10-16 11:38:51 +0500

    update CI/CD

---
+++ target/debug/deps/rust_CI_CD-689ae5ff88713df4.d
@@ -0,0 +1,5 @@
+/home/ismoilovdev/Desktop/Project/rust_CI_CD/target/debug/deps/rust_CI_CD-689ae5ff88713df4.rmeta: src/lib.rs
+
+/home/ismoilovdev/Desktop/Project/rust_CI_CD/target/debug/deps/rust_CI_CD-689ae5ff88713df4.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/rust_CI_CD-be2db6aed0d89f74.d
@@ -0,0 +1,5 @@
+/home/ismoilovdev/Desktop/Project/rust_CI_CD/target/debug/deps/rust_CI_CD-be2db6aed0d89f74.rmeta: src/lib.rs
+
+/home/ismoilovdev/Desktop/Project/rust_CI_CD/target/debug/deps/rust_CI_CD-be2db6aed0d89f74.d: src/lib.rs
+
+src/lib.rs:
